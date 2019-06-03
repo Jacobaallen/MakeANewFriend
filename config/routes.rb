23619e75844e28c7bb028af6a7cefd937a670bcc
Jacob Allen <boat.mon@gmail.com>
2019-06-03 17:21:47 -0400

    added converstion controller to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  #root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :conversations do
+    resources :messages
+  end
+
   root to: 'welcome#show'
 end
